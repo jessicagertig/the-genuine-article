1566d0a757a98d89f3fb16d90b3eb697311db291
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-05 20:34:08 -0500

    Add dropdown menu lookups.

---
+++ src/utils/lookups.ts
@@ -0,0 +1,398 @@
+export type Option = { value: number; label: string }
+
+export const colorOptions: Option[] = [
+  {
+      value: 1,
+      label: "black"
+  },
+  {
+      value: 2,
+      label: "white"
+  },
+  {
+      value: 3,
+      label: "beige"
+  },
+  {
+      value: 4,
+      label: "tan"
+  },
+  {
+      value: 5,
+      label: "grey"
+  },
+  {
+      value: 6,
+      label: "silver"
+  },
+  {
+      value: 7,
+      label: "orange"
+  },
+  {
+      value: 8,
+      label: "red"
+  },
+  {
+      value: 9,
+      label: "maroon"
+  },
+  {
+      value: 10,
+      label: "purple"
+  },
+  {
+      value: 11,
+      label: "lavender"
+  },
+  {
+      value: 12,
+      label: "fuchsia"
+  },
+  {
+      value: 13,
+      label: "pink"
+  },
+  {
+      value: 14,
+      label: "forest green"
+  },
+  {
+      value: 15,
+      label: "green"
+  },
+  {
+      value: 16,
+      label: "lime"
+  },
+  {
+      value: 17,
+      label: "chartreuse"
+  },
+  {
+      value: 18,
+      label: "olive"
+  },
+  {
+      value: 19,
+      label: "yellow"
+  },
+  {
+      value: 20,
+      label: "gold"
+  },
+  {
+      value: 21,
+      label: "navy"
+  },
+  {
+      value: 22,
+      label: "blue"
+  },
+  {
+      value: 23,
+      label: "teal"
+  },
+  {
+      value: 24,
+      label: "aqua"
+  },
+  {
+      value: 25,
+      label: "royal blue"
+  },
+  {
+      value: 26,
+      label: "sky blue"
+  },
+  {
+      value: 27,
+      label: "brown"
+  },
+  {
+      value: 28,
+      label: "lilac"
+  },
+  {
+      value: 29,
+      label: "cream"
+  }
+]
+
+export const materialOptions: Option[] = [
+  {
+      value: 1,
+      label: "cotton"
+  },
+  {
+      value: 2,
+      label: "muslin"
+  },
+  {
+      value: 3,
+      label: "calico"
+  },
+  {
+      value: 4,
+      label: "chintz"
+  },
+  {
+      value: 5,
+      label: "dimity"
+  },
+  {
+      value: 6,
+      label: "organza"
+  },
+  {
+      value: 7,
+      label: "gauze"
+  },
+  {
+      value: 8,
+      label: "cotton gauze"
+  },
+  {
+      value: 9,
+      label: "silk gauze"
+  },
+  {
+      value: 10,
+      label: "lawn"
+  },
+  {
+      value: 11,
+      label: "cotton lawn"
+  },
+  {
+      value: 12,
+      label: "linen lawn"
+  },
+  {
+      value: 13,
+      label: "silk"
+  },
+  {
+      value: 14,
+      label: "linen"
+  },
+  {
+      value: 15,
+      label: "cambric"
+  },
+  {
+      value: 16,
+      label: "flannel"
+  },
+  {
+      value: 17,
+      label: "wool"
+  },
+  {
+      value: 18,
+      label: "worsted"
+  },
+  {
+      value: 19,
+      label: "canvas"
+  },
+  {
+      value: 20,
+      label: "straw"
+  },
+  {
+      value: 21,
+      label: "mother of pearl"
+  },
+  {
+      value: 22,
+      label: "jet"
+  },
+  {
+      value: 23,
+      label: "pearls"
+  },
+  {
+      value: 24,
+      label: "satin"
+  },
+  {
+      value: 25,
+      label: "silk damask"
+  },
+  {
+      value: 26,
+      label: "silk brocade"
+  },
+  {
+      value: 27,
+      label: "tassels"
+  },
+  {
+      value: 28,
+      label: "lace"
+  },
+  {
+      value: 29,
+      label: "whale bone"
+  },
+  {
+      value: 30,
+      label: "steel"
+  },
+  {
+      value: 31,
+      label: "silver"
+  },
+  {
+      value: 32,
+      label: "gold"
+  }
+]
+
+export const garmentTitleOptions: Option[] = [
+  {
+      value: 1,
+      label: "Dress"
+  },
+  {
+      value: 2,
+      label: "Day Dress"
+  },
+  {
+      value: 3,
+      label: "Evening Dress"
+  },
+  {
+      value: 4,
+      label: "Dinner Dress"
+  },
+  {
+      value: 5,
+      label: "Court Dress"
+  },
+  {
+      value: 6,
+      label: "Morning Dress"
+  },
+  {
+      value: 7,
+      label: "Mourning Dress"
+  },
+  {
+      value: 8,
+      label: "Visiting Dress"
+  },
+  {
+      value: 9,
+      label: "Riding Habit (Dress)"
+  },
+  {
+      value: 10,
+      label: "Walking Dress"
+  },
+  {
+      value: 11,
+      label: "Promenade Dress"
+  },
+  {
+      value: 12,
+      label: "Carriage Dress"
+  },
+  {
+      value: 13,
+      label: "Pelisse"
+  },
+  {
+      value: 14,
+      label: "Jacket"
+  },
+  {
+      value: 15,
+      label: "Cape"
+  },
+  {
+      value: 16,
+      label: "Tippet"
+  },
+  {
+      value: 17,
+      label: "Sleeves"
+  },
+  {
+      value: 18,
+      label: "Cloak"
+  },
+  {
+      value: 19,
+      label: "Spencer"
+  },
+  {
+      value: 20,
+      label: "Shawl"
+  },
+  {
+      value: 21,
+      label: "Hat"
+  },
+  {
+      value: 22,
+      label: "Bonnet"
+  },
+  {
+      value: 23,
+      label: "Cap"
+  },
+  {
+      value: 24,
+      label: "Collar"
+  },
+  {
+      value: 25,
+      label: "Blouse"
+  },
+  {
+      value: 26,
+      label: "Shirt Waist"
+  },
+  {
+      value: 27,
+      label: "Over Dress"
+  },
+  {
+      value: 28,
+      label: "Slip"
+  },
+  {
+      value: 29,
+      label: "Petticoat"
+  },
+  {
+      value: 30,
+      label: "Stays"
+  },
+  {
+      value: 31,
+      label: "Corset"
+  },
+  {
+      value: 32,
+      label: "Bustle"
+  },
+  {
+      value: 33,
+      label: "Chemise"
+  },
+  {
+      value: 34,
+      label: "Dressing Gown"
+  },
+  {
+      value: 35,
+      label: "Crinoline"
+  },
+  {
+      value: 36,
+      label: "Hoop Skirt"
+  }
+]

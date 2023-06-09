e701b321c7b7c7fde419f0ee1034062911c7fd81
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-08 21:37:47 -0500

    Add notes re typescript.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -22,13 +22,13 @@ export async function post<T>(endpoint: string, body: object): Promise<T> {
   const url = baseUrl + endpoint;
   return request<T>(url, {
     method: 'POST',
-    body: JSON.stringify(body);
+    body: JSON.stringify(body),
     headers: { 'Content-Type': 'application/json' },
   });
 }
 
 export async function put<T>(endpoint: string, body: object): Promise<T> {
-  const url = baseurl + endpoint;
+  const url = baseUrl + endpoint;
   return request<T>(url, {
     method: 'PUT',
     body: JSON.stringify(body),
@@ -40,3 +40,21 @@ export async function deleteRequest<T>(endpoint: string): Promise<T> {
   const url = baseUrl + endpoint;
   return request<T>(url, { method: 'DELETE' });
 }
+
+/*
+T is used as a type variable, also known as a generic type parameter. Generics are a way to provide type information to a function or a class so that it can work with different types of data while still providing type safety.
+
+In our apiService, we use the generic type parameter T in the request, get, post, put, and deleteRequest functions to indicate that these functions can return data of any type, depending on how they are called.
+
+When calling one of these functions, TypeScript will infer the type of T based on the context in which the function is called. For example, if you expect the get function to return an array of objects with a specific shape, you can use the type variable T to represent that specific shape:
+
+interface User {
+  id: number;
+  name: string;
+}
+
+async function fetchUsers() {
+  const users = await get<User[]>('/users');
+  // Now, TypeScript knows that `users` is of type `User[]`
+}
+*/

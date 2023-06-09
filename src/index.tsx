e687a2559dd7dfa5779a8a1dc8901aecb1cee9c8
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-08 21:47:07 -0500

    Add react-query and QueryClientProvider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,43 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import App from 'src/layouts/AppWithLayout';
 import myTheme from "src/styles/theme";
 import customTheme from "src/styles/customTheme";
 import reportWebVitals from 'src/reportWebVitals';
-import { ThemeProvider } from '@mui/material';
-import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import './styles/styles.scss'
+
+
 
 const rootElement = document.getElementById("root")!; // non null assertion operator tells TS that element will always exist - may need to explicetly type instead when using some eslint extensions
 const root = createRoot(rootElement);
 
+/* REACT QUERY
+--===================================================-- */
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 root.render(
-  <StrictMode>
-    <Router>
-      <ThemeProvider theme={customTheme}>
-        <EmotionThemeProvider theme={myTheme}>
-          <App />
-        </EmotionThemeProvider>
-      </ThemeProvider>
-    </Router>
-  </StrictMode>
+  <QueryClientProvider client={queryClient} >
+    <StrictMode>
+      <Router>
+        <ThemeProvider theme={customTheme}>
+          <EmotionThemeProvider theme={myTheme}>
+            <App />
+          </EmotionThemeProvider>
+        </ThemeProvider>
+      </Router>
+    </StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

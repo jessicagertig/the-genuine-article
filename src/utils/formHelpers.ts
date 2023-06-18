755edf3d23050f3035ff7622fe0b1b83ae072bdb
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-17 21:32:12 -0500

    Add helpers for Dayjs formatting.

---
+++ src/utils/formHelpers.ts
@@ -0,0 +1,20 @@
+import dayjs, { Dayjs } from "dayjs";
+
+export type Unit = "year" | "decade";
+
+export const stringToDate = (unit: Unit, input: string): Dayjs => {
+  if (unit === "year") {
+    const year = input + "-01-01";
+    return dayjs(year);
+  } else {
+    return dayjs();
+  }
+};
+
+export const dateToString = (unit: Unit, date: Dayjs): string => {
+  if (unit === "year") {
+    return date.format("YYYY");
+  } else {
+    return date.format();
+  }
+};

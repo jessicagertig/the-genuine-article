9430fd803ec6df9bdd2b86c4c94f50d9b4d1dd62
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-17 21:30:58 -0500

    Refactor root component to use AppWrapper.

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "@mui/material";
-import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "src/layouts/AppWithLayout";
-import myTheme from "src/styles/theme";
-import customTheme from "src/styles/customTheme";
+import AppWrapper from "./AppWrapper";
 import reportWebVitals from "src/reportWebVitals";
 import "./styles/styles.scss";
 
 const rootElement = document.getElementById("root")!; // non null assertion operator tells TS that element will always exist - may need to explicetly type instead when using some eslint extensions
 const root = createRoot(rootElement);
 
-/* REACT QUERY
---===================================================-- */
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+const WrappedApp = AppWrapper(App);
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <Router>
-        <ThemeProvider theme={customTheme}>
-          <EmotionThemeProvider theme={myTheme}>
-            <App />
-          </EmotionThemeProvider>
-        </ThemeProvider>
-      </Router>
-    </StrictMode>
-  </QueryClientProvider>
-);
+root.render(<WrappedApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

f5579c64276d9fea9bebb3c9e738122914a0a822
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-23 16:56:25 -0500

    Further override mui form field styling.

--- src/components/AdminPage/StyledFields.tsx
+++ src/components/AdminPage/StyledFields.tsx
@@ -1,31 +1,62 @@
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { styled } from "@mui/system";
+import { styled, alpha } from "@mui/system";
 
-export const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
-  width: "100%",
-  margin: "8px 0",
-  "& .MuiFilledInput-root": { color: theme.palette.primary.main },
-  "& input:-webkit-autofill": {
-    WebkitTextFillColor: theme.palette.primary.main,
-  },
-}));
+const createRootStyles = (theme: any): any => {
+  return {
+    width: "100%",
+    margin: "8px 0",
+    "& .MuiFilledInput-root": {
+      color: theme.palette.primary.main,
+      backgroundColor: "rgba(175, 187, 207, 0.2)",
+      "&:hover": {
+        backgroundColor: "rgba(175, 187, 207, 0.4)",
+        "&:before": {
+          borderBottom: `1px solid ${alpha(theme.palette.primary.main, 0.9)}`,
+        },
+      },
+      "&:before": {
+        borderBottom: `1px solid ${alpha(theme.palette.primary.main, 0.66)}`,
+      },
+    },
+    // "& .MuiIconButton-root": {
+    //   color: `${alpha(theme.palette.primary.main, 0.66)}`
+    // },
+    "& input:-webkit-autofill": {
+      WebkitTextFillColor: theme.palette.primary.main,
+    },
+    "& input:-internal-autofill-selected": {
+      backgroundColor: "rgba(175, 187, 207, 0.2)",
+    },
+  };
+};
 
-export const StyledTextField = styled(TextField)(({ theme }) => ({
-  width: "100%",
-  margin: "8px 0",
-  "& .MuiFilledInput-root": { color: theme.palette.primary.main },
-  "& input:-webkit-autofill": {
-    WebkitTextFillColor: theme.palette.primary.main,
-  },
-}));
+export const StyledAutocomplete = styled(Autocomplete)(({ theme }) =>
+  createRootStyles(theme)
+);
 
-export const StyledDatePicker = styled(DatePicker)(({ theme }) => ({
-  width: "100%",
-  margin: "8px 0",
-  "& .MuiFilledInput-root": { color: theme.palette.primary.main },
-  "& input:-webkit-autofill": {
-    WebkitTextFillColor: theme.palette.primary.main,
-  },
-}));
+export const StyledTextField = styled(TextField)(({ theme }) =>
+  createRootStyles(theme)
+);
+
+export const StyledDatePicker = styled(DatePicker)(({ theme }) =>
+  createRootStyles(theme)
+);
+
+// SOME MUI ROOT STYLE CLASSNAMES AS COPIED - most properties removed
+
+// MuiInputBase-root-MuiFilledInput-root:hover:not(.Mui-disabled, .Mui-error):before {
+//   border-bottom: 1px solid rgba(0, 0, 0, 0.87);
+// }
+
+// css-1rgy9zg-MuiInputBase-root-MuiFilledInput-root:before {
+//   border-bottom: 1px solid rgba(0, 0, 0, 0.42);
+// }
+
+// .css-1xpn62i-MuiFormLabel-root-MuiInputLabel-root {
+//   color: rgba(0, 0, 0, 0.6);
+//   font-family: Bellota Text,sans-serif;
+//   z-index: 1;
+//   pointer-events: none;
+// }

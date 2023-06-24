c1dfa4aecbd0e26101ecba00c5fa33e1814691a5
Jessica Cooper <jessicagcooper@gmail.com>
2023-06-24 13:48:37 -0500

    Implement upload file query.

--- src/components/AdminPage/ImageUploadModal.tsx
+++ src/components/AdminPage/ImageUploadModal.tsx
@@ -1,32 +1,80 @@
 import React from "react";
 import DialogModal from "src/components/shared/DialogModal";
 import FileUpload from "src/components/shared/FileUpload";
+import OutlinedButton from "src/components/shared/OutlinedButton";
 
 import { useModalContext } from "src/context/ModalContext";
+
+import { useCreateMainImage } from "src/queryHooks/useImages";
 
 interface ImageUploadModalProps {
   onCancel: () => void;
   onConfirm?: () => void;
+  id: number;
 }
 
 const ImageUploadModal = (props: ImageUploadModalProps) => {
+  console.log("Image Upload Modal Props", props);
+
+  const { id } = props;
+  
+  const { mutate: createMainImage } = useCreateMainImage();
 
   const { modalOpen } = useModalContext();
 
-  // const onConfirm = () => {
-  //   props.onCancel()
-  // }
+  const [imageFile, setImageFile] = React.useState<File | null>(null);
 
-  const title = "Upload Image"
-  console.log("Image Upload Props", props)
+  const handleChangeFileInput = (event: React.BaseSyntheticEvent) => {
+    const file = event.target?.files ? event.target.files[0] : null;
+    console.log("IMAGE FILE", file);
+    setImageFile(file);
+  };
+
+  const handleConfirm = async () => {
+    if (imageFile) {
+      let formData = new FormData();
+      formData.append("image", imageFile);
+
+      // for (const [key, value] of formData.entries()) {
+      //   console.log(`LOGGED ${key}:`, value);
+      // }
+
+      createMainImage(
+        { formData, id },
+        {
+          onSuccess: (data: any) => {
+            console.log("DATA", data);
+          },
+          onError: (error: any) => {
+            console.log("ERROR", error);
+          },
+        }
+      );
+    }
+    props.onCancel(); //removes modal
+  };
+
+  const confirmButton = (
+    <OutlinedButton onClick={handleConfirm}>Upload now</OutlinedButton>
+  );
+
+  const title = "Select Image";
+
   return (
     <>
-      <DialogModal open={modalOpen} dialogTitle={title} onConfirm={props.onCancel} onCancel={props.onCancel}>
-        <FileUpload />
+      <DialogModal
+        open={modalOpen}
+        dialogTitle={title}
+        onCancel={props.onCancel}
+        confirmButton={confirmButton}
+      >
+        <FileUpload
+          handleChangeInput={handleChangeFileInput}
+          fileName={imageFile?.name}
+        />
       </DialogModal>
     </>
-  )
-
+  );
 };
 
 export default ImageUploadModal;

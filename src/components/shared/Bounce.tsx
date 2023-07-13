a84c7e90b2c26c288ecd08d2d621524ba4f5ddc7
Jessica Cooper <jessicagcooper@gmail.com>
2023-07-12 19:14:49 -0500

    Begin creating a bouncing component.

---
+++ src/components/shared/Bounce.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const BouncingComponent: React.FC = () => {
+  const style = useSpring({
+    from: { y: 50 },
+    to: { y: 70 },
+    config: { mass: 1, tension: 10, friction: 0, clamp: true},
+    loop: { reverse: true },
+  });
+
+  return <animated.div style={{ ...style, width: "70px", height: "70px" }} ><ExpandMoreIcon sx={{ fontSize: "65px" }}/></animated.div>;
+};
+
+export default BouncingComponent;

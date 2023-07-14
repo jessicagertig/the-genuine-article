9c838a51930fe11ed4ac6b737ef4931d3193979f
Jessica Cooper <jessicagcooper@gmail.com>
2023-07-14 16:03:26 -0500

    Use custom icon & add circle to bounce element.

--- src/components/shared/Bounce.tsx
+++ src/components/shared/Bounce.tsx
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
 import { useSpring, animated } from "@react-spring/web";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CarrotIcon from "src/components/shared/CarrotIcon";
 
 const BouncingComponent: React.FC = () => {
   const style = useSpring({
-    from: { y: 50 },
-    to: { y: 70 },
-    config: { mass: 1, tension: 10, friction: 0, clamp: true},
+    from: { y: -5 },
+    to: { y: 7 },
+    config: { mass: 1, tension: 10, friction: 0, clamp: true },
     loop: { reverse: true },
   });
 
-  return <animated.div style={{ ...style, width: "70px", height: "70px" }} ><ExpandMoreIcon sx={{ fontSize: "65px", color: "white" }}/></animated.div>;
+  return (
+    <Styled.Circle>
+      <animated.div style={{ ...style, width: "65px", height: "65px" }}>
+        <CarrotIcon />
+      </animated.div>
+    </Styled.Circle>
+  );
 };
+
+// Styled Components
+// =======================================================
+let Styled: any;
+Styled = {};
+
+Styled.Circle = styled.div(() => {
+  return css`
+    border: 2px solid white;
+    border-radius: 50%;
+    width: 75px;
+    height: 75px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+});
 
 export default BouncingComponent;

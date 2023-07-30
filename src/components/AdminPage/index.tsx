5a9561c83fd2861a2f4373bf8293f7da2d2c94a7
Jessica Cooper <jessicagcooper@gmail.com>
2023-07-30 17:26:52 -0500

    Use AdminHeader in AdminPage.

--- src/components/AdminPage/index.tsx
+++ src/components/AdminPage/index.tsx
@@ -2,42 +2,16 @@ import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
-import SecondaryNav from "src/components/shared/SecondaryNav";
 import GarmentsTable from "src/components/AdminPage/GarmentsTable";
-import AddGarmentModal from "src/components/AdminPage/AddGarmentModal";
-import OutlinedButton from "src/components/shared/OutlinedButton";
-
-import { useModalContext } from "src/context/ModalContext";
+import AdminPageHeader from "src/components/AdminPage/AdminPageHeader";
 
 interface AdminPageProps {}
 
 const AdminPage: React.FC<AdminPageProps> = props => {
-  const { openModal, removeModal } = useModalContext();
-
-  const handleClickAdd = (event: React.SyntheticEvent): void => {
-    event.preventDefault();
-    const modal = <AddGarmentModal onCancel={() => removeModal()} />;
-
-    openModal(modal);
-  };
 
   return (
     <Styled.AdminPageContainer>
-      <SecondaryNav pageTitle="Garments" />
-      <Styled.ButtonContainer>
-        <OutlinedButton
-          hasStartIcon={true}
-          iconType="add"
-          onClick={handleClickAdd}
-          styles={{
-            maxWidth: "100px",
-            paddingRight: "8px",
-            paddingLeft: "8px",
-          }}
-        >
-          Add
-        </OutlinedButton>
-      </Styled.ButtonContainer>
+      <AdminPageHeader pageTitle="Garments" />
       <Styled.GarmentsTableContainer>
         <GarmentsTable />
       </Styled.GarmentsTableContainer>
@@ -59,26 +33,6 @@ Styled.AdminPageContainer = styled.div(() => {
     flex-direction: column;
     align-items: center;
     margin-bottom: 100px;
-  `;
-});
-
-Styled.AdminPageHeader = styled.div(props => {
-  const t = props.theme;
-  return css`
-    ${t.pt(6)}
-    label: AdminPageHeader;
-    width: 100%;
-    height: 48px;
-    margin-bottom: 24px;
-    display: flex;
-    justify-content: center;
-
-    h2 {
-      font-family: "bellota text";
-      font-size: 24px;
-      font-weight: 700;
-      color: ${t.color.blue[700]};
-    }
   `;
 });
 

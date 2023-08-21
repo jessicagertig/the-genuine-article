6670a98e1deea6eec429277a5f28b6a18ac70c40
Jessica Cooper <jessicagcooper@gmail.com>
2023-08-20 19:13:40 -0500

    useAuthedUser queryhook

--- src/queryHooks/useAuth.tsx
+++ src/queryHooks/useAuth.tsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import {
   useMutation,
-  // useQuery,
-  // useQueryClient,
-  // QueryClient,
+  useQuery,
 } from "react-query";
-import { apiPost } from "./api";
+import { apiGet, apiPost } from "./api";
 
 type LoginParams = {
   email: string;
@@ -14,7 +13,13 @@ type LoginParams = {
 const loginUser = async ({ email, password }: LoginParams) => {
   return await apiPost({
     endpoint: "/auth/login",
-    variables:{ email, password },
+    variables: { email, password },
+  });
+};
+
+const getAuthedUser = async () => {
+  return await apiGet({
+    endpoint: "/auth",
   });
 };
 
@@ -38,4 +43,28 @@ function useLoginUser(): {
   });
 }
 
-export { useLoginUser };
+function useAuthedUser({ enabled = true }: { enabled?: boolean } = {}): {
+  status: any;
+  data: any;
+  error: any;
+  refetch: any;
+  isFetching: boolean;
+  isLoading: boolean;
+} {
+  return useQuery("authedUser", getAuthedUser,
+    { 
+      enabled,
+      refetchOnWindowFocus: true,
+      retry: (failureCount, error) => !error.message.includes(401),
+      onError: (error) => {
+          console.log("%c[useGetMe] ERROR - Redirecting to /logout", "color: #FF0000");
+          const path = window.location.pathname;
+          if (path.includes("admin")) {
+            window.location.href = `${process.env.REACT_APP_BASE_URL}/login`
+          }
+        }
+      },
+  );
+}
+
+export { useLoginUser, useAuthedUser };

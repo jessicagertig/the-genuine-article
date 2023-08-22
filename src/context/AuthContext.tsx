670965a32374770f148ed41fae43ffcc22045ab0
Jessica Cooper <jessicagcooper@gmail.com>
2023-08-21 21:26:53 -0500

    Remove all but storing currentUser from authContext.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-
-import { useLoginUser } from "src/queryHooks/useAuth";
 
 interface CurrentUser {
   username: string;
   email: string;
 }
 
-interface LoginData {
-  user: CurrentUser;
-  token: string;
-}
-
-interface LoginParams {
-  email: string;
-  password: string;
-}
-
 interface AuthContextProps {
-  isAuthenticated: boolean;
-  currentUser: any;
-  login: (loginParams: LoginParams) => void;
-  logout: () => void;
+  currentUser?: CurrentUser | null;
 }
 
 const AuthContext = React.createContext<AuthContextProps | undefined>(
@@ -31,66 +15,22 @@ const AuthContext = React.createContext<AuthContextProps | undefined>(
 
 interface AuthProviderProps {
   children: React.ReactNode;
+  currentUser?: CurrentUser | null;
 }
 
-const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [currentUser, setCurrentUser] = React.useState<CurrentUser | null>(
-    null
-  );
-  const navigate = useNavigate();
-
-  const { mutate: loginUser, isLoading, error } = useLoginUser();
-
-  const isAuthenticated = !!currentUser;
-
-  const login = async (loginParams: LoginParams) => {
-    // todo: add validation with Yup
-    try {
-      loginUser(
-        {
-          email: loginParams.email,
-          password: loginParams.password,
-        },
-        {
-          onSuccess: (data: LoginData) => {
-            console.log("Success logging in user. Data:", data?.user);
-            const token = data.token;
-            if (data.token) {
-              localStorage.setItem("token", token);
-            }
-            setCurrentUser({
-              username: data?.user?.username,
-              email: data?.user?.email,
-            });
-            navigate("/admin");
-          },
-          onError: (error: any) => {
-            const message = error && error.data ? error.data.message : "";
-            console.log("Request Error:", message);
-          },
-        }
-      );
-    } catch (e) {
-      console.error("ERROR:", e);
-    }
-  };
-
-  const logout = () => {
-    // Remove the token from local storage
-    localStorage.removeItem("token");
-    // Clear the current user
-    setCurrentUser(null);
-    // Redirect the user to a different page
-    navigate("/");
-  };
+const AuthProvider: React.FC<AuthProviderProps> = ({
+  children,
+  currentUser,
+}) => {
+  React.useEffect(() => {
+    console.log("AUTH CONTEXT");
+    console.log("current user", currentUser);
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider
       value={{
-        isAuthenticated,
         currentUser,
-        login,
-        logout,
       }}
     >
       {children}
@@ -101,18 +41,13 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 const useAuthContext = () => {
   const context = React.useContext(AuthContext);
   if (context === undefined) {
-    throw new Error(
-      "useAuthContext must be used within a AuthProvider"
-    );
+    throw new Error("useAuthContext must be used within a AuthProvider");
   }
 
-  const { isAuthenticated, currentUser, login, logout } = context;
+  const { currentUser } = context;
 
   return {
-    isAuthenticated,
     currentUser,
-    login,
-    logout,
   };
 };
 

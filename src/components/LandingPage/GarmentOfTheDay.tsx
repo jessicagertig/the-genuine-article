b7fa211e3007b2b54057ea36dfc056be8eaf81b9
Jessica Cooper <jessicagcooper@gmail.com>
2023-09-12 11:40:48 -0500

    add animation to daily garment

--- src/components/LandingPage/GarmentOfTheDay.tsx
+++ src/components/LandingPage/GarmentOfTheDay.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import { useSpring, useTransition, animated, to } from "@react-spring/web";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 import Skeleton from "@mui/material/Skeleton";
@@ -14,6 +15,7 @@ import GarmentZoomModal from "src/components/Garment/GarmentZoomModal";
 
 import { useModalContext } from "src/context/ModalContext";
 import { useDailyGarment } from "src/queryHooks/useGarments";
+import useIntersectionObserver from "src/hooks/useIntersectionObserver";
 import useImageDimensions from "src/hooks/useImageDimensions";
 
 interface HomeContentProps {
@@ -24,7 +26,6 @@ interface HomeContentProps {
 const HomeContent: React.FC<HomeContentProps> = ({ windowHeight }) => {
   const { openModal, removeModal } = useModalContext();
   const { data: garment } = useDailyGarment();
-  const navigate = useNavigate();
 
   const [imageLoaded, setImageLoaded] = React.useState(false);
   const [dimensions, setDimensions] = React.useState({
@@ -32,11 +33,36 @@ const HomeContent: React.FC<HomeContentProps> = ({ windowHeight }) => {
     width: 0,
   });
 
+  const theme = useTheme();
+  const mediumScreen = useMediaQuery(theme.breakpoints.down("md"));
+
   const { maxHeight } = useImageDimensions({
     imageLoaded,
     dimensions,
   });
   const imgRef = React.useRef<HTMLImageElement>(null!);
+  const triggerRef = React.useRef<HTMLDivElement>(null!);
+  const dataRef = useIntersectionObserver(triggerRef, {
+    freezeOnceVisible: true,
+  });
+
+  const resize = useSpring({
+    from: { scale: 1, translate: "0%" },
+    to: async (next, cancel) => {
+      await next({ scale: dataRef?.isIntersecting ? 0.85 : 1 });
+      await next({ translate: dataRef?.isIntersecting ? "-7.5%" : "0%" });
+    },
+  });
+
+  const no = windowHeight * -0.14
+
+  const transitions = useTransition(dataRef?.isIntersecting, {
+    from: { opacity: 0, transform: `translateY(0px) scale(1)` },
+    enter: { opacity: 1, transform: `translateY(${no}px) scale(1.1)` },
+    leave: { opacity: 1, transform: `translateY(${no}px) scale(1.1)` },
+    unique: true,
+    delay: 800,
+  });
 
   const onLoad = () => {
     setDimensions({
@@ -53,9 +79,6 @@ const HomeContent: React.FC<HomeContentProps> = ({ windowHeight }) => {
     }
   }, [imgRef]);
 
-  const theme = useTheme();
-  const fullscreen = useMediaQuery(theme.breakpoints.down("md"));
-
   const imageUrl =
     garment && garment.imageUrls ? garment.imageUrls.mainImageUrl : "";
   const noImage = imageUrl === "" || imageUrl === undefined;
@@ -66,48 +89,87 @@ const HomeContent: React.FC<HomeContentProps> = ({ windowHeight }) => {
         onClose={() => removeModal()}
         garmentTitle={garment?.garmentTitle}
         imageUrl={imageUrl}
-        responsiveFullscreen={fullscreen}
+        responsiveFullscreen={mediumScreen}
       />
     );
 
     openModal(modal);
   };
 
-  const garmentInfo = (
-    <Styled.Info>
-      <Styled.HeaderContainer>
-        <Styled.InfoTitleContainer>
-          <Styled.InfoTitle>{garment?.garmentTitle}</Styled.InfoTitle>
-        </Styled.InfoTitleContainer>
-        <Styled.IconButtonContainer>
-          <Link to={`/garments/${garment?.id}`} target="_blank">
-            <IconButton
-              sx={{ color: "#020b1c", height: "32px", width: "32px" }}
-            >
-              <OpenInNewOutlinedIcon />
-            </IconButton>
-          </Link>
-        </Styled.IconButtonContainer>
-      </Styled.HeaderContainer>
-      <Styled.InfoItem>
-        <p>
-          c. {garment?.beginYear},<span> {garment?.cultureCountry}</span>
-        </p>
-      </Styled.InfoItem>
-      <Styled.InfoItem>
-        <p className="date"></p>
-      </Styled.InfoItem>
-    </Styled.Info>
-  );
+  const style = mediumScreen
+    ? {}
+    : {
+        transform: to(
+          [
+            resize.translate.to((v: any) => `translateY(${v})`),
+            resize.scale.to((v: any) => `scale(${v})`),
+          ],
+          (translate, scale) => `${translate} ${scale}`
+        ),
+      };
+
+  // const styleTwo = mediumScreen
+  // ? {}
+  // : {
+  //     transform: to(
+  //       [
+  //         appear.translate.to((v: any) => `translateY(${v})`),
+  //       ],
+  //       (translate) => `${translate}`
+  //     ),
+  //     opacity: to([appear.opacity.to((v: any) => `opacity(${v})`),], (opacity) => opacity)
+  //   };
+
+  const garmentInfo = () => {
+    const content = (
+      <>
+        <Styled.HeaderContainer>
+          <Styled.InfoTitleContainer>
+            <Styled.InfoTitle>{garment?.garmentTitle}</Styled.InfoTitle>
+          </Styled.InfoTitleContainer>
+          <Styled.IconButtonContainer>
+            <Link to={`/garments/${garment?.id}`} target="_blank">
+              <IconButton
+                sx={{ color: "#020b1c", height: "32px", width: "32px" }}
+              >
+                <OpenInNewOutlinedIcon />
+              </IconButton>
+            </Link>
+          </Styled.IconButtonContainer>
+        </Styled.HeaderContainer>
+        <Styled.InfoItem>
+          <p>
+            c. {garment?.beginYear},<span> {garment?.cultureCountry}</span>
+          </p>
+        </Styled.InfoItem>
+        <Styled.InfoItem>
+          <p className="date"></p>
+        </Styled.InfoItem>
+      </>
+    );
+    if (!mediumScreen) {
+      return transitions((style, item) =>
+        item ? <Styled.Info style={style}>{content}</Styled.Info> : null
+      );
+    } else {
+      return <Styled.Info>{content}</Styled.Info>
+    }
+  };
+
   // Image container can NOT be conditionally displayed (even if loading is slow)
   // because the imageRef cannot be used until img is rendered (don't forget!)
   return (
     <Styled.HomeContentContainer height={windowHeight}>
       <Styled.ContentTitleContainer>
+        {/* <animated.h2 style={{
+            transform: to([
+              resize.scale.to((v: any) => `scale(${v})`)
+            ], (scale) => `${scale}`),
+            }}>Garment of the Day</animated.h2> */}
         <h2>Garment of the Day</h2>
       </Styled.ContentTitleContainer>
-      <Styled.Card>
-        <Styled.ImageSection>
+      <Styled.Card height={windowHeight}>
+        <Styled.ImageSection style={style}>
           {noImage || !imageLoaded ? (
             <Skeleton
               variant="rectangular"
@@ -144,8 +206,9 @@ const HomeContent: React.FC<HomeContentProps> = ({ windowHeight }) => {
             </div>
           </Styled.DisplayedImage>
         </Styled.ImageSection>
-        {garmentInfo}
+        {garmentInfo()}
       </Styled.Card>
+      <div ref={triggerRef} style={{ marginBottom: "48px" }} />
     </Styled.HomeContentContainer>
   );
 };
@@ -166,7 +229,6 @@ Styled.HomeContentContainer = styled.div((props: any) => {
     width: 100%;
     height: ${heightInVh}vh;
     align-items: center;
-    justify-content: center;
   `;
 });
 
@@ -194,7 +256,7 @@ Styled.Card = styled.div((props: any) => {
   return css`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    align-items: space-between;
     background-color: #ffffff;
     border-radius: 8px;
     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
@@ -202,7 +264,6 @@ Styled.Card = styled.div((props: any) => {
     max-height: calc(${heightInVh}vh - 120px);
     border-radius: 8px;
     width: fit-content;
-    margin-bottom: 48px;
     height: fit-content;
 
     ${t.mq.xs} {
@@ -210,20 +271,28 @@ Styled.Card = styled.div((props: any) => {
     }
 
     ${t.mq.md} {
-      max-width: 640px;
+      box-shadow: none;
+      align-items: flex-start;
+      border-radius: 0px;
     }
   `;
 });
 
-Styled.ImageSection = styled.section(() => {
+Styled.ImageSection = styled(animated.div)((props: any) => {
+  const t = props.theme;
   return css`
     label: DailyGarment_ImageSection;
     display: flex;
     justify-content: center;
+    z-index: 1;
+
+    ${t.mq.md} {
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    }
   `;
 });
 
-Styled.DisplayedImage = styled.div((props: any) => {
+Styled.DisplayedImage = styled(animated.div)((props: any) => {
   const t = props.theme;
   const heightInVh = props.height / (props.height * 0.01);
   const display = props.noImage ? "none" : "flex";
@@ -238,6 +307,7 @@ Styled.DisplayedImage = styled.div((props: any) => {
 
     ${t.mq.md} {
       max-width: 640px;
+      max-height: calc(${heightInVh}vh - 120px);
     }
 
     img {
@@ -247,6 +317,7 @@ Styled.DisplayedImage = styled.div((props: any) => {
 
       ${t.mq.md} {
         max-width: 640px;
+        max-height: calc(${heightInVh}vh - 120px);
       }
     }
 
@@ -265,13 +336,22 @@ Styled.DisplayedImage = styled.div((props: any) => {
   `;
 });
 
-Styled.Info = styled.div(() => {
+Styled.Info = styled(animated.div)(props => {
+  const t = props.theme;
   return css`
     display: flex;
     height: 88px;
     align-items: space-between;
     flex-direction: column;
     padding: 8px;
+    width: 100%;
+    z-index: 0;
+
+    ${t.mq.md} {
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+      padding-top: 24px;
+      height: 140px;
+    }
   `;
 });
 

b5a2f7ea8c8621676ac94d8014830706bf977c6f
Jessica Cooper <jessicagcooper@gmail.com>
2023-11-06 10:54:27 -0600

    progressiveImg transition & formatting

--- src/components/shared/ProgressiveImage.tsx
+++ src/components/shared/ProgressiveImage.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
-interface ProgressiveImgProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+interface ProgressiveImgProps
+  extends React.ImgHTMLAttributes<HTMLImageElement> {
   placeholderSrc: string;
   src: string;
 }
 
-const ProgressiveImg: React.FC<ProgressiveImgProps> = ({ placeholderSrc, src, ...props }) => {
-  const [imgSrc, setImgSrc] = React.useState(placeholderSrc || src)
+const ProgressiveImg: React.FC<ProgressiveImgProps> = ({
+  placeholderSrc,
+  src,
+  ...props
+}) => {
+  const [imgSrc, setImgSrc] = React.useState(placeholderSrc || src);
 
   React.useEffect(() => {
     const img = new Image();
@@ -18,8 +23,7 @@ const ProgressiveImg: React.FC<ProgressiveImgProps> = ({ placeholderSrc, src, ..
     };
   }, [src]);
 
-  const isLoadingState =
-    placeholderSrc && imgSrc === placeholderSrc;
+  const isLoadingState = placeholderSrc && imgSrc === placeholderSrc;
 
   return (
     <Styled.Wrapper isLoading={isLoadingState}>
@@ -51,11 +55,11 @@ Styled.Wrapper = styled.div((props: any) => {
       right: 0;
       bottom: 0;
       left: 0;
-      backdrop-filter: ${props.isLoading ? 'blur(7px)' : 'blur(0px)'};
-      transition: ${props.isLoading ? 'none' : 'filter 0.5s linear'};
+      backdrop-filter: ${props.isLoading ? "blur(7px)" : "blur(0px)"};
+      transition: ${props.isLoading ? "none" : "backdrop-filter 0.5s linear"};
     }
-  `
-})
+  `;
+});
 
 Styled.Image = styled.img((props: any) => {
   return css`
@@ -66,6 +70,6 @@ Styled.Image = styled.img((props: any) => {
     height: 100%;
     width: 100%;
     background-color: #162c6b;
-    clip-path: ${props.isLoading ? 'inset(0)' : "none"};
+    clip-path: ${props.isLoading ? "inset(0)" : "none"};
   `;
 });

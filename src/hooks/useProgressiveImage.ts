16a45a01da4d28d9e9780a860e3a191ecb384e0b
Jessica Cooper <jessicagcooper@gmail.com>
2023-12-01 17:24:53 -0600

    Try different image loading

--- src/hooks/useProgressiveImage.ts
+++ src/hooks/useProgressiveImage.ts
@@ -1,35 +1,75 @@
 import React from "react";
 
-const getDataUri = (url: string): Promise<string> => {
-  const image = new Image();
-  return new Promise((accept) => {
-    image.crossOrigin = "anonymous"
-    image.onload = function () {
-      const canvas = document.createElement("canvas");
-      canvas.width = image.naturalWidth;
-      canvas.height = image.naturalHeight;
+// const getDataUri = (url: string): Promise<string> => {
+//   const image = new Image();
+//   return new Promise((accept) => {
+//     image.crossOrigin = "anonymous"
+//     image.onload = function () {
+//       const canvas = document.createElement("canvas");
+//       canvas.width = image.naturalWidth;
+//       canvas.height = image.naturalHeight;
   
-      canvas.getContext("2d")?.drawImage(image, 0, 0);
+//       canvas.getContext("2d")?.drawImage(image, 0, 0);
     
-      // get as Data URI
-      const dataUri = canvas.toDataURL("image/png");
+//       // get as Data URI
+//       const dataUri = canvas.toDataURL("image/png");
 
-      accept(dataUri)
-    };
-    image.src = url;
-  })
-}
+//       accept(dataUri)
+//     };
+//     image.src = url;
+//   })
+// }
 
-export const useProgressiveImage = (placeholderSrc: string, fullSrc: string) => {
+// export const useProgressiveImage = (placeholderSrc: string, fullSrc: string) => {
+//   const [src, setSrc] = React.useState(placeholderSrc);
+//   console.log("HOOK", { src })
+//   React.useEffect(() => {
+//     const loadImage = async () => {
+//       const uriSrc = await getDataUri(fullSrc);
+//       setSrc(uriSrc);
+//     };
+//     loadImage();
+//   }, [fullSrc]);
+
+//   return src;
+// }
+
+const getDataUri = async (
+  url: string,
+  signal: AbortSignal
+): Promise<string> => {
+  const response = await fetch(url, { signal });
+  const blob = await response.blob();
+  return URL.createObjectURL(blob);
+};
+
+export const useProgressiveImage = (
+  placeholderSrc: string,
+  fullSrc: string
+) => {
   const [src, setSrc] = React.useState(placeholderSrc);
-  console.log("HOOK", { src })
+
   React.useEffect(() => {
-    const loadImage = async () => {
-      const uriSrc = await getDataUri(fullSrc);
-      setSrc(uriSrc);
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const load = async () => {
+      try {
+        const dataUri = await getDataUri(fullSrc, signal);
+        setSrc(dataUri);
+      } catch (error: any) {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      }
     };
-    loadImage();
+
+    load();
+
+    return () => {
+      controller.abort();
+    };
   }, [fullSrc]);
 
   return src;
-}
+};

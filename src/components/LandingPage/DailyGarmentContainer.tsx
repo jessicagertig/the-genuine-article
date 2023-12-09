629ec137f2ef1059da72f334e2b632afc88d11eb
Jessica Cooper <jessicagcooper@gmail.com>
2023-12-09 15:09:58 -0600

    initial fix for daily garment loading

--- src/components/LandingPage/DailyGarmentContainer.tsx
+++ src/components/LandingPage/DailyGarmentContainer.tsx
@@ -19,6 +19,7 @@ const HomeContentContainer: React.FC<HomeContentContainerProps> = ({
 
   const height = windowHeight ?? 100;
   const isVeryShortScreen = height <= 630;
+  const isShortScreen = height <= 800;
   const heightInVh = height / (height * 0.01);
 
   return (
@@ -28,7 +29,14 @@ const HomeContentContainer: React.FC<HomeContentContainerProps> = ({
     >
       <>
         {!garment ? (
-          <DailyGarmentSkeleton />
+          <Styled.SubContainer
+            isVeryShortScreen={isVeryShortScreen}
+            heightInVh={heightInVh}
+          >
+            <Styled.ContentContainer isShortScreen={isShortScreen}>
+              <DailyGarmentSkeleton />
+            </Styled.ContentContainer>
+          </Styled.SubContainer>
         ) : (
           <DailyGarment
             windowHeight={windowHeight}
@@ -48,7 +56,7 @@ export default HomeContentContainer;
 let Styled: any;
 Styled = {};
 
-type Props = { theme: Theme; isVeryShortScreen: boolean; heightInVh: number };
+type Props = { theme: Theme; isVeryShortScreen: boolean; isShortScreen: boolean, heightInVh: number };
 
 Styled.Container = styled.div(
   ({ theme, heightInVh, isVeryShortScreen }: Props) => {
@@ -65,6 +73,69 @@ Styled.Container = styled.div(
       ${t.mq.md} {
         min-height: ${isVeryShortScreen ? "630px" : "unset"};
       }
+
+      ${t.mq.xl} {
+        background-color: white;
+      }
     `;
   }
 );
+
+Styled.SubContainer = styled.div(
+  ({ theme, heightInVh, isVeryShortScreen }: Props) => {
+    const t = theme;
+    return css`
+      label: DailyGarment_SubContainer;
+      display: flex;
+      width: 100%;
+      min-height: min(${heightInVh}vh, 800px);
+      align-items: center;
+      justify-content: center;
+      background-color: white;
+
+      ${t.mq.md} {
+        min-height: ${isVeryShortScreen
+          ? "630px"
+          : `min(${heightInVh}vh, 800px)`};
+      }
+
+      ${t.mq.xl} {
+        min-height: min(${heightInVh}vh, 800px);
+      }
+    `;
+  }
+);
+
+Styled.ContentContainer = styled.div(({ theme, isShortScreen }: Props)=> {
+  const t = theme;
+  
+  return css`
+    label: DailyGarment_HomeContentContainer;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-start;
+    max-width: 1500px;
+
+    ${t.mq.md} {
+      height: ${isShortScreen ? "100%" : "94%"};
+      justify-content: ${isShortScreen ? "space-between" : "flex-start"};
+      ${t.pb(0)};
+    }
+
+    ${t.mq.xl} {
+      height: 100%;
+      padding-right: 2%;
+      padding-left: 2%;
+      flex-direction: row;
+      justify-content: center;
+    }
+
+    ${t.mq.gxl} {
+      padding-right: 5%;
+      padding-left: 5%;
+    }
+  `;
+});

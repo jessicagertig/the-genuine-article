d48836a9c35d4d4519a0f64f1af69099f42010d0
Jessica Cooper <jessicagcooper@gmail.com>
2024-01-01 14:58:26 -0600

    Create string helper fn & validation for garment

--- src/utils/validationWithYup.ts
+++ src/utils/validationWithYup.ts
@@ -1,5 +1,37 @@
 import * as Yup from "yup";
+import { camelToHumanReadable } from "src/utils/helpers";
 
+const createStringRule = (
+  fieldName: string,
+  type = "default"
+) => {
+  const name = camelToHumanReadable(fieldName);
+  let rule = Yup.string()
+    .required(`${name} is required`)
+    .test(fieldName, `${name} is invalid`, (value, context) => {
+      if (value) {
+        return true;
+      } else {
+        return context.createError({ path: fieldName });
+      }
+    });
+
+  switch (type) {
+    case "email":
+      rule = rule.email("Please enter a valid email");
+      break;
+    case "url":
+      rule = rule.url("Please enter a valid URL");
+      break;
+    default:
+      break;
+  }
+
+  return rule;
+};
+
+/* Login Schema & Validation Function
+__________________________________________ */
 export const logInSchema = Yup.object()
   .shape({
     email: Yup.string()
@@ -45,6 +77,8 @@ export const validateLoginField = async (
   return errorMessage;
 };
 
+/* Url Schema & Validation Function
+__________________________________________ */
 const urlOnlySchema = Yup.object().shape({
   url: Yup.string().url("Please enter a valid URL").required("URL is required"),
 });
@@ -62,4 +96,37 @@ export const validateUrl = async (url: string): Promise<string> => {
   return errorMessage;
 };
 
+/* Garment Schema & Validation Function
+__________________________________________ */
 
+const garmentSchema = Yup.object()
+  .shape({
+    garmentTitle: createStringRule("garmentTitle"),
+    beginYear: createStringRule("beginYear"),
+    cultureCountry: createStringRule("cultureCountry"),
+    collection: createStringRule("collection"),
+    collectionUrl: createStringRule("collectionUrl", "url"),
+    itemCollectionNo: createStringRule("itemCollectionNo"),
+  })
+  .noUnknown();
+
+export type GarmentValues = Yup.InferType<typeof garmentSchema>;
+
+export type GarmentField = { key: keyof GarmentValues; value: string };
+
+export const validateGarmentField = async (
+  field: GarmentField
+): Promise<string> => {
+  let errorMessage = "";
+  try {
+    await (garmentSchema.fields[field.key] as Yup.StringSchema).validate(
+      field.value
+    );
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      console.log(`${field.key} Error`, { error });
+      errorMessage = error.message;
+    }
+  }
+  return errorMessage;
+};

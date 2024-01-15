c9c76b1f1d03d49eee682853e79763fa323df2be
Jessica Cooper <jessicagcooper@gmail.com>
2024-01-15 11:40:46 -0600

    Add addGarmentTitle to useMenus queryhooks.

--- src/queryHooks/useMenus.ts
+++ src/queryHooks/useMenus.ts
@@ -1,13 +1,23 @@
 import {
   useQuery,
+  useQueryClient,
+  useMutation,
+  QueryClient,
   UseQueryResult
 } from "react-query";
-import { apiGet } from "./api";
-import { Menus } from "src/utils/formHelpers";
+import { apiGet, apiPost } from "./api";
+import { Menus, Title } from "src/utils/formHelpers";
 
 const getMenus = async (): Promise<Menus> => {
   return await apiGet({ endpoint: "/items/menus" });
 };
+
+const addGarmentTitle = async ({ garmentTitle }: { garmentTitle: string }): Promise<Title> => {
+  return await apiPost({
+    endpoint: "/items/garment_titles",
+    variables: { garmentTitle },
+  });
+}
 
 /* Hooks
 --===================================================-- */
@@ -18,4 +28,22 @@ function useMenus(): UseQueryResult<Menus, string> {
   });
 }
 
-export { useMenus };
+
+function useAddGarmentTitle(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  // console.log("QUERY HOOK");
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(addGarmentTitle, {
+    onSuccess: (data, variables) => {
+      console.log("VARIABLES", variables);
+      console.log("DATA", data);
+      queryClient.invalidateQueries(["menus"]);
+    },
+  });
+}
+
+export { useMenus, useAddGarmentTitle };

5c3c1d7b808422972e444150b362680247a111af
Jessica Cooper <jessicagcooper@gmail.com>
2024-01-17 21:02:09 -0600

    Use delete Garment Title

--- src/queryHooks/useMenus.ts
+++ src/queryHooks/useMenus.ts
@@ -3,22 +3,36 @@ import {
   useQueryClient,
   useMutation,
   QueryClient,
-  UseQueryResult
+  UseQueryResult,
 } from "react-query";
-import { apiGet, apiPost } from "./api";
+import { apiGet, apiPost, apiDelete } from "./api";
 import { Menus, Title } from "src/utils/formHelpers";
 
 const getMenus = async (): Promise<Menus> => {
   return await apiGet({ endpoint: "/items/menus" });
 };
 
-const addGarmentTitle = async ({ garmentTitle }: { garmentTitle: string }): Promise<Title> => {
+const addGarmentTitle = async ({
+  garmentTitle,
+}: {
+  garmentTitle: string;
+}): Promise<Title> => {
   return await apiPost({
     endpoint: "/items/garment_titles",
     variables: { garmentTitle },
   });
-}
+};
 
+const deleteGarmentTitle = async ({
+  garmentTitleId,
+}: {
+  garmentTitleId: number;
+}) => {
+  return await apiDelete({
+    endpoint: `/items/garment_titles/${garmentTitleId}`,
+    variables: {},
+  });
+};
 /* Hooks
 --===================================================-- */
 
@@ -27,7 +41,6 @@ function useMenus(): UseQueryResult<Menus, string> {
     refetchOnWindowFocus: false,
   });
 }
-
 
 function useAddGarmentTitle(): {
   mutate: any;
@@ -46,4 +59,21 @@ function useAddGarmentTitle(): {
   });
 }
 
-export { useMenus, useAddGarmentTitle };
+function useDeleteGarmentTitle(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(deleteGarmentTitle, {
+    onSuccess: (data: any) => {
+      console.log("DATA", data);
+      queryClient.invalidateQueries(["menus"]);
+      // queryClient.invalidateQueries(["garment", data.id]);
+    },
+  });
+}
+
+
+export { useMenus, useAddGarmentTitle, useDeleteGarmentTitle };

e74e071652ef6e31435ae7bcf12c549e16c91693
Jessica Cooper <jessicagcooper@gmail.com>
2024-01-18 12:34:39 -0600

    Add delete & post for color options

--- src/queryHooks/useMenus.ts
+++ src/queryHooks/useMenus.ts
@@ -6,33 +6,45 @@ import {
   UseQueryResult,
 } from "react-query";
 import { apiGet, apiPost, apiDelete } from "./api";
-import { Menus, Title } from "src/utils/formHelpers";
+import { Menus, Title, Color } from "src/utils/formHelpers";
 
 const getMenus = async (): Promise<Menus> => {
   return await apiGet({ endpoint: "/items/menus" });
 };
 
-const addGarmentTitle = async ({
-  garmentTitle,
+const addGarmentTitleOption = async ({
+  garmentTitleOption,
 }: {
-  garmentTitle: string;
+  garmentTitleOption: string;
 }): Promise<Title> => {
   return await apiPost({
     endpoint: "/items/garment_titles",
-    variables: { garmentTitle },
+    variables: { garmentTitleOption },
   });
 };
 
-const deleteGarmentTitle = async ({
-  garmentTitleId,
+const deleteGarmentTitleOption = async ({
+  garmentTitleOptionId,
 }: {
-  garmentTitleId: number;
+  garmentTitleOptionId: number;
 }) => {
   return await apiDelete({
-    endpoint: `/items/garment_titles/${garmentTitleId}`,
+    endpoint: `/items/garment_titles/${garmentTitleOptionId}`,
     variables: {},
   });
 };
+
+const addColorOption = async ({
+  colorOption,
+}: {
+  colorOption: string;
+}): Promise<Color> => {
+  return await apiPost({
+    endpoint: "/colors",
+    variables: { colorOption },
+  });
+};
+
 /* Hooks
 --===================================================-- */
 
@@ -42,7 +54,7 @@ function useMenus(): UseQueryResult<Menus, string> {
   });
 }
 
-function useAddGarmentTitle(): {
+function useAddGarmentTitleOption(): {
   mutate: any;
   status: any;
   error: any;
@@ -50,7 +62,7 @@ function useAddGarmentTitle(): {
 } {
   // console.log("QUERY HOOK");
   const queryClient: QueryClient = useQueryClient();
-  return useMutation(addGarmentTitle, {
+  return useMutation(addGarmentTitleOption, {
     onSuccess: (data, variables) => {
       console.log("VARIABLES", variables);
       console.log("DATA", data);
@@ -59,14 +71,14 @@ function useAddGarmentTitle(): {
   });
 }
 
-function useDeleteGarmentTitle(): {
+function useDeleteGarmentTitleOption(): {
   mutate: any;
   status: any;
   error: any;
   isLoading: boolean;
 } {
   const queryClient: QueryClient = useQueryClient();
-  return useMutation(deleteGarmentTitle, {
+  return useMutation(deleteGarmentTitleOption, {
     onSuccess: (data: any) => {
       console.log("DATA", data);
       queryClient.invalidateQueries(["menus"]);
@@ -75,5 +87,22 @@ function useDeleteGarmentTitle(): {
   });
 }
 
+function useAddColorOption(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  // console.log("QUERY HOOK");
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(addColorOption, {
+    onSuccess: (data, variables) => {
+      console.log("VARIABLES", variables);
+      console.log("DATA", data);
+      queryClient.invalidateQueries(["menus"]);
+    },
+  });
+}
 
-export { useMenus, useAddGarmentTitle, useDeleteGarmentTitle };
+
+export { useMenus, useAddGarmentTitleOption, useDeleteGarmentTitleOption, useAddColorOption };

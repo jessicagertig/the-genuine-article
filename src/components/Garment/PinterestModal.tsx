fe31e4f7d26b49df130a196390b1628135e7c434
Jessica Gertig <jessicamgertig@gmail.com>
2024-07-08 20:30:49 -0500

    Progress on Pinterest Modal

--- src/components/Garment/PinterestModal.tsx
+++ src/components/Garment/PinterestModal.tsx
@@ -1,197 +1,17 @@
-// import React from "react";
-// import styled from "@emotion/styled";
-// import { css } from "@emotion/react";
-
-// import DialogModal from "src/components/shared/DialogModal";
-// import {
-//   List,
-//   ListItem,
-//   ListItemText,
-//   ListItemAvatar,
-//   Button,
-// } from "@mui/material";
-// import Avatar from "@mui/material/Avatar";
-// import PinterestIcon from "@mui/icons-material/Pinterest";
-
-// import { GarmentData, PinterestBoard } from "src/types";
-
-// interface PinterestModalProps {
-//   garment: GarmentData | null;
-//   boards: PinterestBoard[];
-//   open: boolean;
-//   onCancel: () => void;
-//   onSelectBoard: (boardId: string) => void;
-// }
-
-// const PinterestModal: React.FC<PinterestModalProps> = props => {
-//   const { garment, boards, open, onCancel, onSelectBoard } = props;
-
-//   const handleSelectBoard = (boardId: string) => {
-//     onSelectBoard(boardId);
-//   };
-
-//   return (
-//     <DialogModal
-//       open={open}
-//       dialogTitle="Save to Pinterest"
-//       onCancel={onCancel}
-//       full={true}
-//     >
-//       <Styled.ModalContent>
-//         <Styled.GarmentContainer>
-//           <Styled.GarmentImage src={garment?.imageUrls?.mainImageUrl} />
-//         </Styled.GarmentContainer>
-//         <Styled.BoardsContainer>
-//           <Styled.BoardsTitle>Select a Pinterest Board</Styled.BoardsTitle>
-//           <Styled.BoardsList>
-//             <List sx={{ overflow: "auto", maxHeight: 300, width: "100%" }}>
-//               {boards.map(board => (
-//                 <Styled.ListItemWrapper key={board.id}>
-//                   <ListItem>
-//                     <ListItemAvatar>
-//                       <Avatar src={board.media.image_cover_url} />
-//                     </ListItemAvatar>
-//                     <ListItemText primary={board.name} />
-//                     <Styled.SaveButtonContainer>
-//                       <Button
-//                         variant="contained"
-//                         sx={{
-//                           backgroundColor: "#E60023",
-//                           borderRadius: "20px",
-//                           "&:hover": {
-//                             backgroundColor: "#E60023",
-//                           },
-//                         }}
-//                         onClick={() => handleSelectBoard(board.id)}
-//                       >
-//                         <PinterestIcon sx={{ color: "#FFFFFF" }} />
-//                         Save
-//                       </Button>
-//                     </Styled.SaveButtonContainer>
-//                   </ListItem>
-//                 </Styled.ListItemWrapper>
-//               ))}
-//             </List>
-//           </Styled.BoardsList>
-//         </Styled.BoardsContainer>
-//       </Styled.ModalContent>
-//     </DialogModal>
-//   );
-// };
-
-// export default PinterestModal;
-
-// // Styled Components
-// // =======================================================
-// let Styled: any;
-// Styled = {};
-
-// Styled.ModalContent = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: ModalContent;
-//     ${[t.mx(6), t.my(6)]}
-//     width: 100%;
-//     height: 100%;
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: space-between;
-//     align-items: center;
-//   `;
-// });
-
-// Styled.GarmentContainer = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: GarmentContainer;
-//     width: 40%;
-//     height: 100%;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     align-items: center;
-//   `;
-// });
-
-// Styled.GarmentImage = styled.img(props => {
-//   const t = props.theme;
-//   return css`
-//     label: GarmentImage;
-//     width: 200px;
-//     height: 200px;
-//     object-fit: cover;
-//     object-position: top;
-//     border-radius: 10px;
-//   `;
-// });
-
-// Styled.BoardsContainer = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: BoardsContainer;
-//     width: 60%;
-//     height: 100%;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     align-items: center;
-//     padding: 20px;
-//   `;
-// });
-
-// Styled.BoardsTitle = styled.h3(props => {
-//   const t = props.theme;
-//   return css`
-//     label: BoardsTitle;
-//     text-align: center;
-//     margin-bottom: 10px;
-//   `;
-// });
-
-// Styled.BoardsList = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: BoardsList;
-//     width: 100%;
-//     height: 100%;
-//     padding: 10px;
-//     border-top: 1px solid #ccc;
-//   `;
-// });
-
-// Styled.ListItemWrapper = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: ListItemWrapper;
-//     &:hover {
-//       ${Styled.SaveButtonContainer} {
-//         visibility: visible;
-//       }
-//     }
-//   `;
-// });
-
-// Styled.SaveButtonContainer = styled.div(props => {
-//   const t = props.theme;
-//   return css`
-//     label: SaveButtonContainer;
-//     visibility: hidden;
-//   `;
-// });
-
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
-import {
-  Modal,
-  Box,
-  Typography,
-  Button,
-  TextField,
-  List,
-  ListItemText,
-  ListItem,
-  Avatar,
-} from "@mui/material";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import Avatar from "@mui/material/Avatar";
+import AddIcon from "@mui/icons-material/Add";
+import CancelIcon from "@mui/icons-material/Close";
+import PinterestIcon from "@mui/icons-material/Pinterest";
 import { GarmentData, PinterestBoard } from "src/types";
 import {
   useCreatePinterestBoard,
@@ -199,8 +19,6 @@ import {
   useCreatePinterestPin,
 } from "src/queryHooks/useIntegrations";
 import CreatePinterestBoardForm from "./CreatePinterestBoardForm";
-import AddIcon from "@mui/icons-material/Add";
-import CancelIcon from "@mui/icons-material/Cancel";
 
 interface PinterestModalProps {
   garment: GarmentData | null;
@@ -235,8 +53,17 @@ const StyledImage = styled.img`
 
 const StyledListItem = styled(ListItem)`
   &:hover {
-    background-color: red;
-    color: white;
+    background-color: #f5f5f5;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  visibility: hidden;
+`;
+
+const ListItemWrapper = styled.div`
+  &:hover ${ButtonContainer} {
+    visibility: visible;
   }
 `;
 
@@ -257,12 +84,12 @@ function PinterestModal({
   const { mutate: createPin } = useCreatePinterestPin();
   const [boardId, setBoardId] = React.useState("");
   const [pinId, setPinId] = React.useState("");
-  const [newBoardState, setNewBoardState] = React.useState({
+  const [newBoardState, setNewBoardState] = useState({
     name: "",
     description: "",
     secret: false,
   });
-  const [showForm, setShowForm] = React.useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   const onChangePinterestBoard = (name: string, value: string | boolean) => {
     setNewBoardState(prev => ({ ...prev, [name]: value }));
@@ -316,7 +143,13 @@ function PinterestModal({
     >
       <Box sx={modalStyle}>
         <Box display="flex" flexDirection="row" flex="1" p={2}>
-          <Box flex="1" display="flex" flexDirection="column" width="33%">
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            width="33%"
+            mr={2}
+          >
             <StyledImage src={garment?.imageUrls?.mainImageUrl} alt="Garment" />
             <Typography variant="h6" mt={2}>
               {garment?.garmentTitle}
@@ -325,45 +158,64 @@ function PinterestModal({
               {truncateDescription(garment?.description || "")}
             </Typography>
           </Box>
-          <Box flex="1" display="flex" flexDirection="column" width="67%">
-            {showForm && (
+          <Box
+            flex="1"
+            display="flex"
+            flexDirection="column"
+            width="67%"
+            ml={2}
+          >
+            {showForm ? (
               <CreatePinterestBoardForm
                 onChange={onChangePinterestBoard}
                 newBoardState={newBoardState}
               />
-            )}
-            <TextField fullWidth label="Search" variant="outlined" />
-            <List style={{ overflow: "auto", maxHeight: 300, marginTop: 8 }}>
-              {boards.map(board => (
-                <StyledListItem
-                  key={board.id}
-                  onMouseEnter={() => onSelectBoard(board.id)}
+            ) : (
+              <>
+                <TextField fullWidth label="Search" variant="outlined" />
+                <List
+                  style={{ overflow: "auto", maxHeight: 300, marginTop: 8 }}
                 >
-                  <Avatar
-                    src={board.media.image_cover_url}
-                    variant="rounded"
-                    sx={{ marginRight: 2 }}
-                  />
-                  <ListItemText primary={board.name} />
-                  <Button variant="contained" color="primary">
-                    Pin it
-                  </Button>
-                </StyledListItem>
-              ))}
-            </List>
+                  {boards.map(board => (
+                    <ListItemWrapper key={board.id}>
+                      <StyledListItem
+                        onMouseEnter={() => onSelectBoard(board.id)}
+                      >
+                        <Avatar
+                          src={board.media.image_cover_url}
+                          variant="rounded"
+                          sx={{ marginRight: 2 }}
+                        />
+                        <ListItemText primary={board.name} />
+                        <ButtonContainer>
+                          <Button
+                            variant="contained"
+                            color="error"
+                            sx={{ backgroundColor: "red" }}
+                            startIcon={<PinterestIcon />}
+                          >
+                            Pin it
+                          </Button>
+                        </ButtonContainer>
+                      </StyledListItem>
+                    </ListItemWrapper>
+                  ))}
+                </List>
+              </>
+            )}
           </Box>
         </Box>
         <Box display="flex" justifyContent="space-between" p={2}>
           <Button
             variant="contained"
             color="secondary"
-            onClick={onCancel}
-            startIcon={<CancelIcon />}
+            onClick={showForm ? toggleShowForm : onCancel}
+            endIcon={<CancelIcon />}
           >
-            Cancel
+            {showForm ? "Cancel" : "Close"}
           </Button>
           {showForm ? (
-            <Button variant="contained" color="primary" onClick={onSubmit}>
+            <Button variant="contained" color="error" onClick={onSubmit}>
               Save Board
             </Button>
           ) : (

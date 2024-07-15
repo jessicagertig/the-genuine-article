46c19e1996eabf1ca4a880f94f7a6aa8a0308d9d
Jessica Gertig <jessicamgertig@gmail.com>
2024-07-15 15:43:46 -0500

    Dummy data for Pinterest Boards for testing

---
+++ src/utils/boards.ts
@@ -0,0 +1,201 @@
+import { PinterestBoard } from 'src/types';
+
+export const boards: PinterestBoard[] = [
+  {
+    pin_count: 4,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543400450",
+    created_at: "2017-01-17T05:05:07",
+    board_pins_modified_at: "2020-08-31T01:22:36.621000",
+    name: "Lovely Stuff",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/00/50/e6/0050e6c10ed8cdf5f218d721f96e7fbe.jpg",
+        "https://i.pinimg.com/150x150/1e/44/ab/1e44ab9b5254b628b6979bd18ceacb13.jpg",
+        "https://i.pinimg.com/150x150/5c/d8/e3/5cd8e31375bf1914854584ecc9b021a0.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/c4/fc/38/c4fc381a7df8ecbb0380b83187720d1a.jpg",
+    },
+    follower_count: 36,
+    description: "",
+  },
+  {
+    pin_count: 25,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543402429",
+    created_at: "2017-02-02T03:08:42",
+    board_pins_modified_at: "2021-04-26T19:01:11.208000",
+    name: "Potential Floor Plans",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/72/9b/ec/729bec65b2791cfb186804479b3bae7b.jpg",
+        "https://i.pinimg.com/150x150/87/95/2f/87952f85c63f00669ddf89d637021f8c.jpg",
+        "https://i.pinimg.com/150x150/6f/20/08/6f20086d1c0f1d977b41f202f49466d9.jpg",
+        "https://i.pinimg.com/150x150/23/db/ca/23dbca76506b77e01e146eb136addd03.jpg",
+        "https://i.pinimg.com/150x150/ce/4f/18/ce4f18886f29c436fb227f0b5183487c.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/72/60/58/726058138873228fb3940933b04e23cb.jpg",
+    },
+    follower_count: 36,
+    description: "",
+  },
+  {
+    pin_count: 38,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543364340",
+    created_at: "2016-05-01T01:42:07",
+    board_pins_modified_at: "2018-04-12T02:21:36.681000",
+    name: "pretty picture",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/72/4b/23/724b23561737ca3afe866244874110b0.jpg",
+        "https://i.pinimg.com/150x150/8e/2f/63/8e2f631da9a36915faeaa66d2af73a99.jpg",
+        "https://i.pinimg.com/150x150/71/54/ef/7154efa4b7534f67d8c4dc06541067e2.jpg",
+        "https://i.pinimg.com/150x150/59/86/63/5986635987ff604380bc0853068f4419.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/19/bf/13/19bf135f771d60c56bc3f29f46bb26da.jpg",
+    },
+    follower_count: 37,
+    description: "",
+  },
+  {
+    pin_count: 603,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543186965",
+    created_at: "2012-04-26T18:14:51",
+    board_pins_modified_at: "2023-02-20T21:59:20.871000",
+    name: "Quilting",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/bc/11/4e/bc114e7147bb30f52b1053eca57d03da.jpg",
+        "https://i.pinimg.com/150x150/4f/24/3f/4f243f87e834c57686cde8581b5cef2f.jpg",
+        "https://i.pinimg.com/150x150/60/65/4b/60654b6658291595d2878dd6272f98b4.jpg",
+        "https://i.pinimg.com/150x150/fc/44/42/fc44421cfa4efdef5b8aef62c7651ae6.jpg",
+        "https://i.pinimg.com/150x150/f7/68/a0/f768a01e84bc2319464191f6458e265b.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/09/97/66/099766426233b20a9cf3f992a320a5ae.jpg",
+    },
+    follower_count: 48,
+    description: "",
+  },
+  {
+    pin_count: 20,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543438765",
+    created_at: "2017-10-25T01:25:07",
+    board_pins_modified_at: "2017-10-28T20:24:31.397000",
+    name: "Retreats",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/ba/65/a1/ba65a1026b524a8bbe1411a7c2748468.jpg",
+        "https://i.pinimg.com/150x150/2c/ab/90/2cab902b25beb1080dd0eb4e77d4fe9b.jpg",
+        "https://i.pinimg.com/150x150/8a/47/bd/8a47bdc7a17c6f61edd2c95bd793229d.jpg",
+        "https://i.pinimg.com/150x150/f7/1b/df/f71bdfd2a0c9f4c5769e6ab72a117855.jpg",
+        "https://i.pinimg.com/150x150/bd/44/4c/bd444c55bc52abdf186153ee9adbc973.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/13/81/16/13811678a027c4433fc58b705c5a7471.jpg",
+    },
+    follower_count: 36,
+    description: "",
+  },
+  {
+    pin_count: 445,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543187912",
+    created_at: "2012-05-02T17:38:51",
+    board_pins_modified_at: "2023-03-30T14:42:43.568000",
+    name: "Sewing Stuff",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/c8/c3/24/c8c32425f0b4b904ffae0a0e1589fca4.jpg",
+        "https://i.pinimg.com/150x150/ea/fd/95/eafd953fbd7149535c01afb238bcfe41.jpg",
+        "https://i.pinimg.com/150x150/ec/74/0d/ec740d5cd283ca001e376a0bd7299598.jpg",
+        "https://i.pinimg.com/150x150/e3/86/f8/e386f8d262f212323c43fd292b9d196d.jpg",
+        "https://i.pinimg.com/150x150/04/80/1d/04801d1eda2262092ab713de4550d399.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/custom_covers/400x300/101471866543187912_1384127146.jpg",
+    },
+    follower_count: 52,
+    description: "",
+  },
+  {
+    pin_count: 16,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543283958",
+    created_at: "2015-03-05T05:27:50",
+    board_pins_modified_at: "2020-11-22T17:58:23.518000",
+    name: "Small Space Ideas",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/da/74/7b/da747b3766d22d2fac2e664c474d5d43.jpg",
+        "https://i.pinimg.com/150x150/81/ac/c4/81acc41af144e021c633adf0d1b80643.jpg",
+        "https://i.pinimg.com/150x150/86/fa/b4/86fab42f931ddd7207215d87aa1dc177.jpg",
+        "https://i.pinimg.com/150x150/a0/96/7b/a0967baea50b264920d8ad47220940dc.jpg",
+        "https://i.pinimg.com/150x150/f0/6c/53/f06c535bed1338f83a4f50fa7ed26b49.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/10/86/b2/1086b212bf876a94dfa2e0942b19eb8e.jpg",
+    },
+    follower_count: 36,
+    description: "",
+  },
+  {
+    pin_count: 48,
+    owner: {
+      username: "jessicamgertig",
+    },
+    collaborator_count: 0,
+    id: "101471866543176759",
+    created_at: "2012-03-16T18:31:52",
+    board_pins_modified_at: "2019-06-15T08:27:46.254000",
+    name: "Trying to Cook",
+    privacy: "PUBLIC",
+    media: {
+      pin_thumbnail_urls: [
+        "https://i.pinimg.com/150x150/a1/58/8b/a1588b1d112a5e4c079895fc62e5a4cd.jpg",
+        "https://i.pinimg.com/150x150/fa/2c/63/fa2c63ea2185f6534c5e37d159876f0a.jpg",
+        "https://i.pinimg.com/150x150/45/da/3e/45da3e4eb8981d4cda5e2c06f17b6be8.jpg",
+        "https://i.pinimg.com/150x150/25/d6/de/25d6ded6e2f894fe4de709db25551dac.jpg",
+        "https://i.pinimg.com/150x150/8b/f0/56/8bf0563e1100a9014ee9cf3a0924a2a7.jpg",
+      ],
+      image_cover_url:
+        "https://i.pinimg.com/400x300/10/0f/86/100f868f5ebe724a2d0e7760631f960f.jpg",
+    },
+    follower_count: 35,
+    description: "",
+  },
+];

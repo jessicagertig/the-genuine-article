daa0db4325f08a030e76ccaf4ddd2bb5d3a6726a
Jessica Gertig <jessicamgertig@gmail.com>
2024-07-18 21:29:14 -0500

    Create React query functions & hooks for editing menu options

--- src/queryHooks/useMenus.ts
+++ src/queryHooks/useMenus.ts
@@ -5,7 +5,7 @@ import {
   QueryClient,
   UseQueryResult,
 } from "react-query";
-import { apiGet, apiPost, apiDelete } from "./api";
+import { apiGet, apiPost, apiPut, apiDelete } from "./api";
 import { Menus, Title, Color, Material } from "src/utils/formHelpers";
 
 const getMenus = async (): Promise<Menus> => {
@@ -41,6 +41,47 @@ const addMaterialOption = async ({
 }): Promise<Material> => {
   return await apiPost({
     endpoint: "/items-materials/materials",
+    variables: { materialOption },
+  });
+};
+
+const editGarmentTitleOption = async ({
+  garmentTitleOptionId,
+  garmentTitleOption,
+}: {
+  garmentTitleOptionId: number;
+  garmentTitleOption: string;
+}) => {
+  return await apiPut({
+    endpoint: `/items/garment_titles/${garmentTitleOptionId}`,
+    variables: { garmentTitleOption },
+  });
+};
+
+// Edit color option
+const editColorOption = async ({
+  colorOptionId,
+  colorOption,
+}: {
+  colorOptionId: number;
+  colorOption: string;
+}): Promise<void> => {
+  return await apiPut({
+    endpoint: `/items-colors/colors/${colorOptionId}`,
+    variables: { colorOption },
+  });
+};
+
+// Edit material option
+const editMaterialOption = async ({
+  materialOptionId,
+  materialOption,
+}: {
+  materialOptionId: number;
+  materialOption: string;
+}): Promise<void> => {
+  return await apiPut({
+    endpoint: `/items-materials/materials/${materialOptionId}`,
     variables: { materialOption },
   });
 };
@@ -136,6 +177,84 @@ function useAddMaterialOption(): {
   });
 }
 
+function useEditGarmentTitleOption(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(editGarmentTitleOption, {
+    onSuccess: (data, variables) => {
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Garment Title Option VARIABLES:",
+        { variables }
+      );
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Garment Title Option DATA:",
+        { data }
+      );
+      queryClient.invalidateQueries("menus");
+    },
+  });
+}
+
+function useEditColorOption(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(editColorOption, {
+    onSuccess: (data, variables) => {
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Color Option VARIABLES:",
+        { variables }
+      );
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Color Option DATA:",
+        { data }
+      );
+      queryClient.invalidateQueries("menus");
+    },
+  });
+}
+
+function useEditMaterialOption(): {
+  mutate: any;
+  status: any;
+  error: any;
+  isLoading: boolean;
+} {
+  const queryClient: QueryClient = useQueryClient();
+  return useMutation(editMaterialOption, {
+    onSuccess: (data, variables) => {
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Material Option VARIABLES:",
+        { variables }
+      );
+      console.log(
+        "%cSuccess!",
+        "background-color: green;",
+        "Edit Material Option DATA:",
+        { data }
+      );
+      queryClient.invalidateQueries("menus");
+    },
+  });
+}
+
 function useDeleteGarmentTitleOption(): {
   mutate: any;
   status: any;
@@ -186,6 +305,9 @@ export {
   useAddGarmentTitleOption,
   useAddColorOption,
   useAddMaterialOption,
+  useEditGarmentTitleOption,
+  useEditColorOption,
+  useEditMaterialOption,
   useDeleteGarmentTitleOption,
   useDeleteColorOption,
   useDeleteMaterialOption,
